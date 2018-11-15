84012be196f075fcdff0e1721bc664c4ac70fd88
yanyy <yanyy@allinfinance.com>
2018-11-15 12:52:03 +0800

    my-sites

---
+++ src/main/java/com/cn/mysites/MySitesApplication.java
@@ -0,0 +1,12 @@
+package com.cn.mysites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySitesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySitesApplication.class, args);
+    }
+}
